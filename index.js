c29edfe0a53cb0c1c59651a733dc3937253b23d2
Aniket Rajput <aniketmakwana1010@gmail.com>
2023-02-21 22:45:09 +0530

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", function(){
 let menu=document.querySelector("#menu-icon");
 let navlist=document.querySelector(".navlist");
 
-menu.onclick = () => {fffv
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open')
 };
@@ -16,5 +16,9 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open')
 };
+// let m1=document.querySelector("m1");
+// m1.onclick=()=>{
+//     window.open('https://drive.google.com/file/d/1szToZaY6vIZeAhKmQN3H4bKXoGiiuTbk/view?usp=share_link')
+// }
 
 
